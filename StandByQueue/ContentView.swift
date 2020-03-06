ef241fbef425b043de38c4caf3d90b7a7b64661a
Stephen Nary <spnary@gmail.com>
2020-03-05 20:38:17 -0500

    Initial Commit

---
+++ StandByQueue/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  StandByQueue
+//
+//  Created by Stephen Nary on 3/5/20.
+//  Copyright © 2020 Stephen Nary. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ StandByQueueTests/StandByQueueTests.swift
@@ -0,0 +1,34 @@
+//
+//  StandByQueueTests.swift
+//  StandByQueueTests
+//
+//  Created by Stephen Nary on 3/5/20.
+//  Copyright © 2020 Stephen Nary. All rights reserved.
+//
+
+import XCTest
+@testable import StandByQueue
+
+class StandByQueueTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
